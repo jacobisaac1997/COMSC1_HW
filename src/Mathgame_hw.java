401610e1ffbf7eb170433c81f5f93633f34b037f
jacob isaac <isaacj@student.swosu.edu>
2015-10-07 09:50:47 -0500

    Created outline of the program

--- src/Mathgame_hw.java
+++ src/Mathgame_hw.java
@@ -1,7 +1,15 @@
 //This program has the user play a math game
+//import scanner library
 public class Mathgame_hw {
-
 	public static void main(String[] args) {
+		//Build scanner object
+		//Define variables
+		//The for loop creates four rounds
+		//Check the users points for difficulty of question
+		//Generate two random numbers
+		//Prompt the user to answer said question
+		//Check answer and give points if answer was correct
+		//Print the users points and if print correct answer if they were wrong
 		
 	}
 }
