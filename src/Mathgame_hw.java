b22d5d0f0275b2c321cf70f8175b4054e848041e
jacob isaac <isaacj@student.swosu.edu>
2015-10-09 09:54:04 -0500

    Added some code

--- src/Mathgame_hw.java
+++ src/Mathgame_hw.java
@@ -6,15 +6,24 @@ public class Mathgame_hw {
 		//Build scanner object
 		//Define variables
 		int r;
-		int points;
+		int points = 0;
 		int userAnswer;
 		int cAnswer;
+		int random1;
+		int random2;
 		//The for loop creates four rounds
-		//Check the users points for difficulty of question
+		for(r = 1;r > 5;r++){
 		//Generate two random numbers
-		//Prompt the user to answer said question
+			random1 = (int)(Math.random() * 10);
+			random2 = (int)(Math.random() * 10);
+		//Check the users points for difficulty of question
+			if(points == 0){
+				//Prompt the user to answer said question
+				System.out.println("What is the answer to " + random1 + random2 + ".");
+			}
 		//Check answer and give points if answer was correct
 		//Print the users points and if print correct answer if they were wrong
+		}
 		//End the program by telling the user how many points they earned
 	}
 }
