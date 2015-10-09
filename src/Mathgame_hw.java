79fa623b292692ba8f946619285c6c822d55b039
jacob isaac <isaacj@student.swosu.edu>
2015-10-09 09:33:55 -0500

    Finished the outline and added variables

--- src/Mathgame_hw.java
+++ src/Mathgame_hw.java
@@ -1,9 +1,14 @@
 //This program has the user play a math game
 //import scanner library
+import java.util.Scanner;
 public class Mathgame_hw {
 	public static void main(String[] args) {
 		//Build scanner object
 		//Define variables
+		int r;
+		int points;
+		int userAnswer;
+		int cAnswer;
 		//The for loop creates four rounds
 		//Check the users points for difficulty of question
 		//Generate two random numbers
